a1f106038511b37e048ae3d2d87b395b0256b8af
Isnardo Corredor <juniorcorredor2@gmail.com>
2025-01-02 14:50:30 -0500

    agregar android

--- appcoordenadas/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'ionic.appcoordenadas',
-  appName: 'appcoordenadas',
-  webDir: 'www/browser',
-};
-
-export default config;
--- appcoordenadas/src/app/home/home.page.ts
+++ appcoordenadas/src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ToastController, IonicModule  } from '@ionic/angular';
+import { Geolocation, PermissionStatus } from '@capacitor/geolocation';
+
 import {
   FormBuilder,
   FormGroup,
@@ -18,14 +20,13 @@ import {
   IonItem,
   IonText,
 } from '@ionic/angular/standalone';
-import { Plugins } from '@capacitor/core';
-
-const { Geolocation } = Plugins;
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  standalone: true,
   imports: [
     IonHeader,
     IonToolbar,
@@ -38,11 +39,17 @@ const { Geolocation } = Plugins;
     IonText,
     ReactiveFormsModule,
     CommonModule,
+    IonicModule,
+    BrowserModule
   ],
 })
 export class HomePage implements OnInit {
   form: FormGroup;
-  constructor(private fb: FormBuilder, private alertController: AlertController) {}
+  constructor(
+    private fb: FormBuilder,
+    private alertController: AlertController,
+    private snackbar: ToastController
+  ) {}
 
   ngOnInit(): void {
     this.initForm();
@@ -59,25 +66,112 @@ export class HomePage implements OnInit {
       const nombre = this.form.controls['nombre'].value;
 
       try {
-        const position = await Geolocation['getCurrentPosition']();
-        const coords = `Latitud: ${position.coords.latitude}, Longitud: ${position.coords.longitude}`;
-        const alert = await this.alertController.create({
-          header: 'Saludo',
-          message: `Hola ${nombre}, estas son tus coordenadas: ${coords}`,
-          buttons: ['OK']
-        });
-        await alert.present();
-      } catch (error) {
-        console.error('Error al obtener las coordenadas', error);
+        // Verificar permisos de geolocalización
+        const permiso = await this.getPermisoGeolocalizacion();
+        console.log('Permiso obtenido:', permiso);
+
+        if (permiso === 'denied') {
+          const alert = await this.alertController.create({
+            header: 'Permiso Requerido',
+            message:
+              'Es necesario otorgar permisos de ubicación para continuar.',
+            buttons: ['OK'],
+          });
+          await alert.present();
+          return;
+        }
+
+        // Obtener coordenadas
+        const coordenadas = await this.getCoordenadasActuales();
+        console.log('Coordenadas:', coordenadas);
+
+        if (coordenadas.latitud && coordenadas.longitud) {
+          console.log('Entrando a crear alerta');
+          const alert = await this.alertController.create({
+            header: 'Saludo',
+            message: `Hola ${nombre}, estas son tus coordenadas: Latitud: ${coordenadas.latitud}, Longitud: ${coordenadas.longitud}`,
+            buttons: ['OK'],
+          });
+          console.log('alerta', alert)
+          await alert.present();
+        } else {
+          throw new Error(
+            coordenadas.mensaje || 'Error desconocido al obtener coordenadas.'
+          );
+        }
+      } catch (error: any) {
+        console.error('Error en la función saludar:', error);
         const alert = await this.alertController.create({
           header: 'Error',
-          message: 'Error al obtener las coordenadas',
-          buttons: ['OK']
+          message:
+            'No se pudo obtener la ubicación. Por favor, inténtalo de nuevo.',
+          buttons: ['OK'],
         });
         await alert.present();
       }
     } else {
       this.form.markAllAsTouched();
+    }
+  }
+
+  async getPermisoGeolocalizacion(): Promise<string> {
+    try {
+      const permiso: PermissionStatus = await Geolocation.checkPermissions();
+      console.log('permiso', permiso);
+      if (permiso.location === 'denied') {
+        const solicitud = await Geolocation.requestPermissions();
+        console.log('solicitud', solicitud);
+        return solicitud.location;
+      }
+      console.log(permiso.location);
+      return permiso.location;
+    } catch (error) {
+      console.error(
+        'Error al comprobar los permisos de geolocalización:',
+        error
+      );
+      return 'denied';
+    }
+  }
+
+  async getCoordenadasActuales(): Promise<{
+    latitud?: number;
+    longitud?: number;
+    mensaje?: string;
+  }> {
+    const tiempoDeEspera = 15000; // 15 segundos
+
+    console.log('tiempodeespera', tiempoDeEspera);
+    const tiempoDeEsperaPromise = new Promise<void>((_, reject) => {
+      setTimeout(() => {
+        reject({ message: 'Tiempo de espera agotado' });
+      }, tiempoDeEspera);
+    });
+    console.log('tiempodeseperapromise', tiempoDeEsperaPromise);
+    try {
+      const coordenadas: any = await Promise.race([
+        Geolocation.getCurrentPosition({
+          enableHighAccuracy: true,
+          maximumAge: 0,
+        }),
+        tiempoDeEsperaPromise,
+      ]);
+      console.log('coordenadas actuales', coordenadas);
+      return {
+        latitud: coordenadas.coords.latitude,
+        longitud: coordenadas.coords.longitude,
+      };
+    } catch (error: any) {
+      console.error('Error al obtener las coordenadas:', error);
+      // Mostrar un toast cuando ocurra un error
+      const toast = await this.snackbar.create({
+        message:
+          'No se ha podido capturar las coordenadas. Inténtelo nuevamente',
+        duration: 5000, // Duración del toast en milisegundos
+        position: 'top', // Posición del toast en la pantalla
+      });
+      toast.present();
+      return { mensaje: error.message || 'Error desconocido' };
     }
   }
 }
